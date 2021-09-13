2f9cb0a7a22596db0eb784d07f84f8a2ea964da8
Nasib Mammadov <bduti09@gmail.com>
2021-09-13 22:35:40 +0400

    fixed

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+
+
+
+// настройка подключения к бд
+
+// инициализируем приложение
+
+// Обьявим наши роуты
